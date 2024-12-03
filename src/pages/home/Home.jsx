4587259a64a862580d12f731a9edd33fd0b983f7
Olanrewaju Kabiru <programmerolakay@gmail.com>
2024-12-03 22:55:20 +0100

    Fix:route apthname extension (#4)
    * Fix:route apthname extension
    
    * Added hero section navigation to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,6 +8,7 @@ import 'swiper/css/navigation';
 
 // Swiper modules
 import { Pagination, Navigation } from 'swiper/modules';
+import Hero from '../../components/Hero';
 
 
 const Home = () => {
@@ -16,33 +17,7 @@ const Home = () => {
   return (
     <div className="font-[Montserrat]">
       
-      <section className="w-full h-[100vh] bg-cover bg-no-repeat flex flex-col justify-between" style={{ backgroundImage: 'url(/images/header.jpeg)' }}>
-        {/* Background Overlay */}
-        <div className="absolute inset-0 bg-black bg-opacity-45 lg:mt-[10rem] mt-[6rem] h-full"></div>
-
-        {/* Content Section */}
-        <div className="relative z-10 h-fit flex flex-col justify-center items-start text-start lg:px-[6.25rem] px-[1rem] lg:space-y-[2.5rem]">
-          <h1 className="text-white text-[1.8rem] lg:text-[3.25rem] font-bold lg:mt-[7rem] mt-[5rem] leading-[2rem] lg:leading-[3.875rem] ">
-            Empowering Individuals & Communities to thrive through Mentorship, Impact Projects & Community Building
-          </h1>
-          <p className="text-white text-lg lg:text-[1.4rem] lg:mt-[1.5rem] mt-[3rem] max-w-2xl">
-            The Jimoh Habibullah Initiative is an NGO dedicated to fostering empathy,
-            empowerment, and community growth. We offer ...
-          </p>
-          <button className="my-[2.125rem] lg:px-[3.06rem] px-[2rem] py-[1.25rem] bg-blue-500 hover:bg-blue-600 text-white rounded-lg font-[700]">
-            Learn More
-          </button>
-        </div>
-
-        {/* Arrows and Tabs Section */}
-        <div className="relative z-10 flex justify-end items-center space-x-[1.3rem] px-4 pb-4">
-          <img src="./images/arrow_left.svg" alt="" className="w-fit" />
-          <img src="./images/tab1.svg" alt="" />
-          <img src="./images/tab2.svg" alt="" />
-          <img src="./images/tab1.svg" alt="" />
-          <img src="./images/arrow_right.svg" alt="" className="w-fit" />
-        </div>
-      </section>
+      <Hero />
 
       <section className='w-full flex justify-between flex-row overflow-x-scroll'>
         <div className='bg-[#258CCF] px-[2.43rem] py-[3rem] '>
